45913503cc0e7219331167be9c708b735c06a83c
Vitor Zanchini Santini <santini.vitor@hotmail.com>
2025-11-17 23:42:53 -0300

    adding httpie and creating routes GET and POST

--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
 import http from 'http';
 
 const server = http.createServer((req, res) => {
+
+  const { method, url } = req;
+
+  if (method == 'GET' && url == '/users'){
+    return res.end('Listagem de Usuários')
+  }
+
+  if (method == 'POST' && url == '/users'){
+    return res.end('Criação de Usuário')
+  }
+
   return res.end('Hello World!');
 })
 
