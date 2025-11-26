eb0f89b7fa9923f7d18c1339d2fe6df519b63e5f
Vitor Zanchini Santini <santini.vitor@hotmail.com>
2025-11-25 23:39:20 -0300

    http codes

--- src/server.js
+++ src/server.js
@@ -1,18 +1,28 @@
 import http from 'http';
 
+const users = [];
+
 const server = http.createServer((req, res) => {
 
   const { method, url } = req;
 
   if (method == 'GET' && url == '/users'){
-    return res.end('Listagem de Usuários')
+    return res
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users));
   }
 
   if (method == 'POST' && url == '/users'){
-    return res.end('Criação de Usuário')
+    users.push({
+      id: 1,
+      name: 'Jonh Doe',
+      email: 'johndoe@example.com'
+    });
+
+    return res.writeHead(201).end();
   }
 
-  return res.end('Hello World!');
+  return res.writeHead(404).end();
 })
 
 server.listen(3333);
